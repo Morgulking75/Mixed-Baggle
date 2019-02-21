9d664f563ca7d36efbd4f9e1e70ba396d087768d
Karl Hoglander <k.hoglander@gmail.com>
2019-02-20 20:22:54 -0500

    Adding a linter.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,11 +21,11 @@ export class HomeComponent {
 	public enteredText: string;
 	public guessedWords: Map<string, number>;
 	public wordList: Map<string, number>;
-	public timeTaken: number = 0;
-	public wordCount: number = 0;
+	public timeTaken = 0;
+	public wordCount = 0;
 	private scoring: Map<number, number>;
-	public totalPoints: number = 0;
-	public showFoundWords: boolean = false;
+	public totalPoints = 0;
+	public showFoundWords = false;
 
 	faAngleDown = faAngleDown;
 	faAngleUp = faAngleUp;
@@ -48,16 +48,16 @@ export class HomeComponent {
 		}
 
 		this.shakenDice = [];
-		let availableLetters = this.settings.dice.dice.slice();
-		let shakerService = new ShakerService(this.settings.seed);
-		let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+		const availableLetters = this.settings.dice.dice.slice();
+		const shakerService = new ShakerService(this.settings.seed);
+		const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 		for (let x = 0; x < this.settings.height; x++) {
-			let col = new Array<Die>();
+			const col = new Array<Die>();
 
 			for (let y = 0; y < this.settings.width; y++) {
 				if (availableLetters.length > 0) {
-					let chosenDie = shakerService.chooseDie(availableLetters);
+					const chosenDie = shakerService.chooseDie(availableLetters);
 
 					availableLetters.filter(function (value, index, arr) {
 						return value === chosenDie;
@@ -65,10 +65,10 @@ export class HomeComponent {
 
 					chosenDie.showing = shakerService.chooseSide(chosenDie);
 					chosenDie.heatMap.clearAll();
-					let split = chosenDie.showing.split("");
+					const split = chosenDie.showing.split("");
 
 					split.forEach(character => {
-						let charIndex = letters.indexOf(character);
+						const charIndex = letters.indexOf(character);
 
 						if (charIndex > -1) {
 							letters.splice(charIndex, 1);
@@ -82,7 +82,7 @@ export class HomeComponent {
 			this.shakenDice.push(col);
 		}
 
-		let start = new Date();
+		const start = new Date();
 
 		let filteredWordList = this.settings.dictionary.trimWordsContainingLetters(letters);
 
@@ -90,11 +90,11 @@ export class HomeComponent {
 
 		let hashTable = this.hashWordList(filteredWordList);
 
-		let traverse = new TraverseService(hashTable, this.shakenDice);
+		const traverse = new TraverseService(hashTable, this.shakenDice);
 
 		this.wordList = traverse.getWordList();
 
-		let end = new Date();
+		const end = new Date();
 
 		this.timeTaken = end.getTime() - start.getTime();
 		this.wordCount = Object.keys(this.wordList).length;
@@ -103,9 +103,9 @@ export class HomeComponent {
 	}
 
 	public showOnBoard = () => {
-		let word = [this.enteredText];
+		const word = [this.enteredText];
 
-		let traverse = new TraverseService(this.hashWordList(word), this.shakenDice);
+		const traverse = new TraverseService(this.hashWordList(word), this.shakenDice);
 
 		traverse.highlightWord();
 	}
@@ -113,8 +113,8 @@ export class HomeComponent {
 	public onKey = ($event) => {
 		if (!this.guessedWords[this.enteredText.toUpperCase()]
 			&& this.wordList[this.enteredText.toUpperCase()]) {
-			let wordLength = this.enteredText.length > 8 ? 8 : this.enteredText.length;
-			let points = this.scoring[wordLength];
+			const wordLength = this.enteredText.length > 8 ? 8 : this.enteredText.length;
+			const points = this.scoring[wordLength];
 
 			this.guessedWords[this.enteredText.toUpperCase()] = points;
 			this.totalPoints += points;
@@ -126,11 +126,11 @@ export class HomeComponent {
 	}
 
 	private hashWordList(wordList: Array<string>): Map<string, HashEnum> {
-		let hashTable = new Map<string, HashEnum>();
+		const hashTable = new Map<string, HashEnum>();
 
 		wordList.forEach(element => {
 			element = element.toUpperCase();
-			let wordArray = element.split("");
+			const wordArray = element.split("");
 			let miniWord = "";
 
 			let result = hashTable[element] || HashEnum.DoesNotExist;
