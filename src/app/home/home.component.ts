c6a6051e2f576ea24729a56c3b4aad9f5b21d822
Karl Hoglander <k.hoglander@gmail.com>
2019-02-20 20:56:14 -0500

    testing some linter items

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,177 +1,176 @@
 import { Component, OnInit } from '@angular/core';
-import { ShakerService } from '../shaker/shaker.service';
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import { Die } from '../die/die';
-import { TraverseService } from '../game-board/traverse.service';
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
-import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import { HashEnum } from '../game-board/hashenum';
+import { TraverseService } from '../game-board/traverse.service';
 import { Settings } from '../settings/settings';
+import { ShakerService } from '../shaker/shaker.service';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-    title = 'mixed-baggle';
+	title = 'mixed-baggle';
 
-    constructor() { }
-    public settings: Settings = new Settings();
-    public shakenDice: Array<Array<Die>> = [];
-    public enteredText: string;
-    public guessedWords: Map<string, number>;
-    public wordList: Map<string, number>;
-    public timeTaken = 0;
-    public wordCount = 0;
-    private scoring: Map<number, number>;
-    public totalPoints = 0;
-    public showFoundWords = false;
+	constructor() { }
+	public settings: Settings = new Settings();
+	public shakenDice: Array<Array<Die>> = [];
+	public enteredText: string;
+	public guessedWords: Map<string, number>;
+	public wordList: Map<string, number>;
+	public timeTaken = 0;
+	public wordCount = 0;
+	private scoring: Map<number, number>;
+	public totalPoints = 0;
+	public showFoundWords = false;
 
-    faAngleDown = faAngleDown;
-    faAngleUp = faAngleUp;
+	faAngleDown = faAngleDown;
+	faAngleUp = faAngleUp;
 
-    ngOnInit() {
-        this.guessedWords = new Map<string, number>();
-        this.wordList = new Map<string, number>();
-        this.scoring = new Map<number, number>();
-        this.scoring[3] = 1;
-        this.scoring[4] = 1;
-        this.scoring[5] = 2;
-        this.scoring[6] = 3;
-        this.scoring[7] = 5;
-        this.scoring[8] = 11;
-    }
+	ngOnInit() {
+		this.guessedWords = new Map<string, number>();
+		this.wordList = new Map<string, number>();
+		this.scoring = new Map<number, number>();
+		this.scoring[3] = 1;
+		this.scoring[4] = 1;
+		this.scoring[5] = 2;
+		this.scoring[6] = 3;
+		this.scoring[7] = 5;
+		this.scoring[8] = 11;
+	}
 
-    public generateBoard = () => {
-        if (this.settings.height * this.settings.width > this.settings.dice.dice.length) {
-            return;
-        }
+	public generateBoard = () => {
+		if (this.settings.height * this.settings.width > this.settings.dice.dice.length) {
+			return;
+		}
 
-        this.shakenDice = [];
-        let availableLetters = this.settings.dice.dice.slice();
-        let shakerService = new ShakerService(this.settings.seed);
-        let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+		this.shakenDice = [];
+		let availableLetters = this.settings.dice.dice.slice();
+		let shakerService = new ShakerService(this.settings.seed);
+		let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-        for (let x = 0; x < this.settings.height; x++) {
-            let col = new Array<Die>();
+		for (let x = 0; x < this.settings.height; x++) {
+			let col = new Array<Die>();
 
-            for (let y = 0; y < this.settings.width; y++) {
-                if (availableLetters.length > 0) {
-                    let chosenDie = shakerService.chooseDie(availableLetters);
+			for (let y = 0; y < this.settings.width; y++) {
+				if (availableLetters.length > 0) {
+					let chosenDie = shakerService.chooseDie(availableLetters);
 
-                    availableLetters.filter(function (value, index, arr) {
-                        return value === chosenDie;
-                    });
+					availableLetters.filter(function (value, index, arr) {
+						return value === chosenDie;
+					});
 
-                    chosenDie.showing = shakerService.chooseSide(chosenDie);
-                    chosenDie.heatMap.clearAll();
-                    let split = chosenDie.showing.split("");
+					chosenDie.showing = shakerService.chooseSide(chosenDie);
+					chosenDie.heatMap.clearAll();
+					let split = chosenDie.showing.split("");
 
-                    split.forEach(character => {
-                        let charIndex = letters.indexOf(character);
+					split.forEach(character => {
+						let charIndex = letters.indexOf(character);
 
-                        if (charIndex > -1) {
-                            letters.splice(charIndex, 1);
-                        }
-                    });
+						if (charIndex > -1) {
+							letters.splice(charIndex, 1);
+						}
+					});
 
-                    col.push(chosenDie);
-                }
-            }
+					col.push(chosenDie);
+				}
+			}
 
-            this.shakenDice.push(col);
-        }
+			this.shakenDice.push(col);
+		}
 
-        let start = new Date();
+		let start = new Date();
 
-        let filteredWordList = this.settings.dictionary.trimWordsContainingLetters(letters);
+		let filteredWordList = this.settings.dictionary.trimWordsContainingLetters(letters);
 
-        filteredWordList = filteredWordList.filter(x => x.length >= this.settings.minlength);
+		filteredWordList = filteredWordList.filter(x => x.length >= this.settings.minlength);
 
-        let hashTable = this.hashWordList(filteredWordList);
+		let hashTable = this.hashWordList(filteredWordList);
 
-        let traverse = new TraverseService(hashTable, this.shakenDice);
+		let traverse = new TraverseService(hashTable, this.shakenDice);
 
-        this.wordList = traverse.getWordList();
+		this.wordList = traverse.getWordList();
 
-        let end = new Date();
+		let end = new Date();
 
-        this.timeTaken = end.getTime() - start.getTime();
-        this.wordCount = Object.keys(this.wordList).length;
+		this.timeTaken = end.getTime() - start.getTime();
+		this.wordCount = Object.keys(this.wordList).length;
 
-        hashTable = new Map<string, number>();
-    }
+		hashTable = new Map<string, number>();
+	}
 
-    public showOnBoard = () => {
-        let word = [this.enteredText];
+	public showOnBoard = () => {
+		let word = [this.enteredText];
 
-        let traverse = new TraverseService(this.hashWordList(word), this.shakenDice);
+		let traverse = new TraverseService(this.hashWordList(word), this.shakenDice);
 
-        traverse.highlightWord();
-    }
+		traverse.highlightWord();
+	}
 
-    public onKey = ($event) => {
-        if (!this.guessedWords[this.enteredText.toUpperCase()]
-            && this.wordList[this.enteredText.toUpperCase()]) {
-            let wordLength = this.enteredText.length > 8 ? 8 : this.enteredText.length;
-            let points = this.scoring[wordLength];
+	public onKey = ($event) => {
+		if (!this.guessedWords[this.enteredText.toUpperCase()]
+			&& this.wordList[this.enteredText.toUpperCase()]) {
+			let wordLength = this.enteredText.length > 8 ? 8 : this.enteredText.length;
+			let points = this.scoring[wordLength];
 
-            this.guessedWords[this.enteredText.toUpperCase()] = points;
-            this.totalPoints += points;
-            this.enteredText = "";
-        }
-        else {
-            $event.target.select();
-        }
-    }
+			this.guessedWords[this.enteredText.toUpperCase()] = points;
+			this.totalPoints += points;
+			this.enteredText = "";
+		}
+		else {
+			$event.target.select();
+		}
+	}
 
-    private hashWordList(wordList: Array<string>): Map<string, HashEnum> {
-        let hashTable = new Map<string, HashEnum>();
+	private hashWordList(wordList: Array<string>): Map<string, HashEnum> {
+		let hashTable = new Map<string, HashEnum>();
 
-        wordList.forEach(element => {
-            element = element.toUpperCase();
-            let wordArray = element.split("");
-            let miniWord = "";
+		wordList.forEach(element => {
+			element = element.toUpperCase();
+			let wordArray = element.split("");
+			let miniWord = "";
 
-            let result = hashTable[element] || HashEnum.DoesNotExist;
+			let result = hashTable[element] || HashEnum.DoesNotExist;
 
-            switch (result) {
-                case HashEnum.DoesNotExist:
-                    hashTable[element] = HashEnum.FullWord;
-                    break;
-                case HashEnum.PartialWord:
-                    hashTable[element] = HashEnum.ContinuingWord;
-                    break;
-                default:
-                    break;
-            }
+			switch (result) {
+				case HashEnum.DoesNotExist:
+					hashTable[element] = HashEnum.FullWord;
+					break;
+				case HashEnum.PartialWord:
+					hashTable[element] = HashEnum.ContinuingWord;
+					break;
+				default:
+					break;
+			}
 
-            for (let x = 0; x < wordArray.length - 1; x++) {
-                miniWord += wordArray[x];
+			for (let x = 0; x < wordArray.length - 1; x++) {
+				miniWord += wordArray[x];
 
-                result = hashTable[miniWord] || HashEnum.DoesNotExist;
+				result = hashTable[miniWord] || HashEnum.DoesNotExist;
 
-                switch (result) {
-                    case HashEnum.DoesNotExist:
-                        hashTable[miniWord] = HashEnum.PartialWord;
-                        break;
-                    case HashEnum.FullWord:
-                        hashTable[miniWord] = HashEnum.ContinuingWord
-                        break;
-                    default:
-                        break;
-                }
-            }
-        });
+				switch (result) {
+					case HashEnum.DoesNotExist:
+						hashTable[miniWord] = HashEnum.PartialWord;
+						break;
+					case HashEnum.FullWord:
+						hashTable[miniWord] = HashEnum.ContinuingWord
+						break;
+					default:
+						break;
+				}
+			}
+		});
 
-        return hashTable;
-    }
+		return hashTable;
+	}
 
-    ascOrder = (a, b) => {
-        if (a.key > b.key) return a.key;
-    }
+	ascOrder = (a, b) => {
+		if (a.key > b.key) return a.key;
+	}
 
-    toArray(obj) {
-        return Object.keys(obj).map((key) => { return { "key": key, "value": obj[key] }; });
-    }
+	toArray(obj) {
+		return Object.keys(obj).map((key) => { return { "key": key, "value": obj[key] }; });
+	}
 }
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,39 +1,39 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Settings } from './settings';
+import { DiceService } from '../dice/dice.service';
 import { DiceCollection } from '../dice/diceCollection';
 import { Dictionary } from '../dictionary/dictionary';
 import { DictionaryService } from '../dictionary/dictionary.service';
-import { DiceService } from '../dice/dice.service';
+import { Settings } from './settings';
 
 @Component({
-    selector: 'app-game-settings',
-    templateUrl: './settings.component.html',
-    styleUrls: ['./settings.component.scss']
+	selector: 'app-game-settings',
+	templateUrl: './settings.component.html',
+	styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
-    @Input() settings: Settings;
+	@Input() settings: Settings;
 
-    public showSettings: false;
-    private maxSeed = 9999999;
-    private dictionaries: Array<Dictionary>;
-    private dices: Array<DiceCollection>;
+	public showSettings: false;
+	private maxSeed = 9999999;
+	private dictionaries: Array<Dictionary>;
+	private dices: Array<DiceCollection>;
 
-    constructor(private dictionaryService: DictionaryService, private diceService: DiceService) { }
+	constructor(private dictionaryService: DictionaryService, private diceService: DiceService) { }
 
-    ngOnInit() {
-        this.settings.seed = Math.floor(Math.random() * this.maxSeed);
+	ngOnInit() {
+		this.settings.seed = Math.floor(Math.random() * this.maxSeed);
 
-        this.dices = this.diceService.getAllCollections();
-        this.dictionaries = this.dictionaryService.getAllCollections();
+		this.dices = this.diceService.getAllCollections();
+		this.dictionaries = this.dictionaryService.getAllCollections();
 
-        if (this.dices.length > 0) {
-            this.settings.dice = this.dices[0];
-            this.settings.height = this.settings.dice.height;
-            this.settings.width = this.settings.dice.width;
-        }
+		if (this.dices.length > 0) {
+			this.settings.dice = this.dices[0];
+			this.settings.height = this.settings.dice.height;
+			this.settings.width = this.settings.dice.width;
+		}
 
-        if (this.dictionaries.length > 0) {
-            this.settings.dictionary = this.dictionaries[0];
-        }
-    }
+		if (this.dictionaries.length > 0) {
+			this.settings.dictionary = this.dictionaries[0];
+		}
+	}
 }
